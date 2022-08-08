f12e230733e1ce2863e8c60b9079457b6653182d
kim-sung-jee <lsvk3577@naver.com>
2022-08-08 16:47:35 +0900

    add : 스웨거설정

---
+++ src/main/java/com/nftfont/module/HelloController.java
@@ -0,0 +1,13 @@
+package com.nftfont.module;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello/api")
+    public String Hello(){
+        return "hello";
+    }
+}
