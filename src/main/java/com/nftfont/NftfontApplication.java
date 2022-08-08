e8c28fcf8431e47505bb6ac047851ea9d8c9c532
kim-sung-jee <lsvk3577@naver.com>
2022-08-08 14:29:58 +0900

    환경설정하기

---
+++ src/main/java/com/nftfont/NftfontApplication.java
@@ -0,0 +1,13 @@
+package com.nftfont;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NftfontApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NftfontApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nftfont/NftfontApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nftfont;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NftfontApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
