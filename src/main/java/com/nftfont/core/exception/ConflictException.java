6d9bf9fde25c1cfb617d5863139d994a59269720
kim-sung-jee <lsvk3577@naver.com>
2022-08-21 01:53:31 +0900

    feat : exception handler 등록

---
+++ src/main/java/com/nftfont/core/exception/ConflictException.java
@@ -0,0 +1,13 @@
+package com.nftfont.core.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String msg, Throwable t) {
+        super(msg,t);
+    }
+    public ConflictException(String msg) {
+        super(msg);
+    }
+    public ConflictException() {
+        super();
+    }
+}
---
+++ src/main/java/com/nftfont/core/exception/ExceptionResponse.java
@@ -0,0 +1,18 @@
+package com.nftfont.core.exception;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class ExceptionResponse {
+    private String type;
+    private String msg;
+
+    public static ExceptionResponse of(Exception exception){
+        return ExceptionResponse.builder()
+                .type(exception.getClass().getSimpleName())
+                .msg(exception.getMessage())
+                .build();
+    }
+}
