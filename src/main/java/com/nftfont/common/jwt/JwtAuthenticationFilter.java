4c0b91f273480b9b846639274c2fb24513719211
sung_jee <lsvk3577@naver.com>
2022-11-09 20:42:54 +0900

    [build]

--- src/main/java/com/nftfont/common/jwt/JwtAuthenticationFilter.java
+++ src/main/java/com/nftfont/common/jwt/JwtAuthenticationFilter.java
@@ -25,6 +25,13 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     protected void doFilterInternal(HttpServletRequest request,
                                     HttpServletResponse response,
                                     FilterChain filterChain) throws ServletException, IOException {
+        Enumeration<String> headerNames = request.getHeaderNames();
+        while(headerNames.hasMoreElements()){
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            System.out.println(key+"ㅁㅁㅁ"+value);
+        }
+
 
         String tokenStr = HeaderUtil.getAccessToken(request);
         JwtToken token =  tokenProvider.convertJwtToken(tokenStr);
